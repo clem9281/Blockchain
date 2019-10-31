f2190d4ff36cd4ddc09d6b3321ae67f6153a921e
clem9281 <clem9281@gmail.com>
2019-10-31 16:06:50 -0700

    adds react app for showing user transactions

--- client_mining_p/blockchain.py
+++ client_mining_p/blockchain.py
@@ -174,3 +174,8 @@ def home():
 # Run the program on port 5000
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
+
+"""
+Holds a sequence of elements and occupies a contiguous block of memory. 
+"""
